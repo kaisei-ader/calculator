53626218f60abb1967e70ab82ceb7029d5c31ec7
kaisei <kaisei101010@icloud.com>
2020-10-05 16:33:00 +0900

    演算子クリックでそれぞれの値番号をメゾット変数に代入しstateの値を変換 2段階目の数字クリック時に別の変数に代入させる

--- js/index.js
+++ js/index.js
@@ -6,6 +6,9 @@ class calculator {
     init(){
         this.field  = "0";
         this.value1 = "";
+        this.value2 = "";
+        this.state = 0;
+        this.method = 0;
     }
 
     set field(value){
@@ -13,8 +16,23 @@ class calculator {
     }
 
     setVal(value){
-        this.value1 = this.value1 + value
-        this.field = this.value1
+        switch(this.state){
+            case 0:
+                console.log(value)
+                this.value1 = this.value1 + value
+                this.field = this.value1
+                break
+            case 1:
+                console.log(value)
+                this.value2 = this.value + value
+                this.field = this.value2
+                break
+        }
+    }
+
+    selectMethod(value){
+    this.method = value
+    this.state = 1
     }
 }
 
