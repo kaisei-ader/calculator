fa0ebde69ffb52d9ec5ff6d8bd9b15fa78c33300
kaisei <kaisei101010@icloud.com>
2020-10-05 16:22:35 +0900

    初期状態　HTMLフィールドに0を表示

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-console.log("ブランチテストjs")
+class calculator {
+        constructor(){
+        this.init()
+    }
+    
+    init(){
+        this.field  = "0";
+        this.value1 = "";
+    }
+
+    set field(value){
+        $(".text1").text(value)
+    }
+}
+
+const c = new calculator();
