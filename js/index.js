10fcdc236a63630fd51f643d681dfebb3082329b
kaisei-ader <66417849+kaisei-ader@users.noreply.github.com>
2020-10-08 01:04:22 +0900

    動き実装 (#2)
    * html onclick要素追加
    
    * ブランチ　テスト
    
    * ブランチ　テスト2
    
    * 初期状態　HTMLフィールドに0を表示
    
    * 初期段階の数字クリック時に文字列で値を出力
    
    * 演算子クリックでそれぞれの値番号をメゾット変数に代入しstateの値を変換
    2段階目の数字クリック時に別の変数に代入させる
    
    * = 押したとき値出力　演算子クリック時にcss適用＆解除
    
    * 演算子クリックでbox-shadowの影を内側にモーション
    
    * 削除、全削除　実装
    =処理後の再度計算　実装
    
    * 小数点　実装
    小数点　2度目のクリック　反応させない
    % 実装
    
    * 一旦完了
    
    * html onclick要素追加
    
    * 削除、全削除　実装
    =処理後の再度計算　実装
    
    * 小数点　実装
    小数点　2度目のクリック　反応させない
    % 実装
    
    * 一旦完了

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 class calculator {
-        constructor(){
+    constructor(){
         this.init()
     }
     
@@ -9,21 +9,106 @@ class calculator {
         this.value2 = "";
         this.state = 0;
         this.method = 0;
+        this.changeClear = true;
     }
 
     set field(value){
         $(".text1").text(value)
     }
+    
+    get field(){
+        return Number($(".text1").text())
+    }
+
+    set changeClear(value){
+        $('#' + (value ? 'c' : 'ac')).css('display', 'none')
+        $('#' + (!value ? 'c' : 'ac')).css('display', 'inline')
+    }
+
+    adDot(){
+        switch(this.state){
+            case 0:
+                let search1 = this.value1.includes('.');
+                if(!search1){
+                    this.value1 = this.field + '.'
+                    this.field = this.value1
+                }
+                break
+            case 1:
+            case 2:
+                let search2 = this.value2.includes('.');
+                if(!search2){
+                    this.value2 = this.field + '.'
+                    this.field = this.value2
+                }
+                break
+        }
+    }
+
+    allClear(){
+        this.init()
+        $('.method').css({'box-shadow':'7px 7px 14px #bec4c9, -7px -7px 14px #fff','transition':'all 0.1s'});
+    }
+
+    clear(){
+        switch(this.state){
+            case 0:
+                let result = this.value1.slice(0, -1);
+                if(result === ''){
+                    this.field = "0"
+                    this.value1 =""
+                    this.changeClear = true
+                } else {
+                    this.value1 = result.toString()
+                    this.field = this.value1
+                }
+                break
+            case 1:
+            case 2:
+                let result2 = this.value2.slice(0, -1);
+                if(result2 === ''){
+                    this.field = "0"
+                    this.value2 =""
+                    this.changeClear = true
+                } else {
+                    this.value2 = result2.toString()
+                    this.field = this.value2
+                }
+                break
+        }
+    }
+
+    changeVal(value){
+        switch(this.state){
+            case 0:
+                this.value1 = value
+                this.field = this.value1
+                break
+            case 1:
+            case 2:
+                this.value2 = value
+                this.field = this.value2
+                break
+            case 3:
+                this.field = value
+        }
+        this.changeClear = true
+    }
 
     setVal(value){
         switch(this.state){
             case 0:
-                console.log(value)
                 this.value1 = this.value1 + value
                 this.field = this.value1
+                this.changeClear = false
                 break
             case 1:
-                console.log(value)
+                this.value2 = value.toString()
+                this.field = this.value2
+                this.state = 2
+                this.changeClear = false
+                break
+            case 2:
                 this.value2 = this.value2 + value
                 this.field = this.value2
                 break
@@ -31,6 +116,11 @@ class calculator {
     }
 
     selectMethod(value){
+        switch(this.state){
+            case 3:
+                this.value1 = this.field.toString()
+                break
+        }
         this.method = value
         this.state = 1
         $('.method').css({'box-shadow':'7px 7px 14px #bec4c9, -7px -7px 14px #fff','transition':'all 0.1s'});
@@ -69,6 +159,8 @@ class calculator {
             }
         }
         calcValue(this.value1,this.value2)
+        this.changeClear = true
+        this.state = 3
     }
 }
 
